260f69b704e8786cdf1dc9de0527d7630293e732
meiji163 <mysatellite99@gmail.com>
2022-07-17 21:55:28 -0700

    add moar img

--- src/juliaset/core.clj
+++ src/juliaset/core.clj
@@ -11,8 +11,10 @@
     (recur f (- n 1) (f z))
     z))
 
-
-(defn iter-while< [thresh f n ^Complex z]
+(defn iter-while<
+  "iterate func f for n steps, or until |z|>thresh.
+  Returns the number of iterations"
+  [thresh f n ^Complex z]
   (letfn [(iter [z i]
             (if (or
                  (>= i n)
@@ -21,34 +23,18 @@
               (recur (f z) (+ 1 i))))]
     (iter z 0)))
 
-
-(defn scc [^Complex z]
-  (let [c (Complex. 0.99 0.14)]
-    (C/times z (C/plus z c))))
-
-
-(defn rabbit [^Complex z]
-  (let [c (Complex. -0.122 0.745)]
-    (C/plus (C/times z z) c)))
-
-
-(defn grid [xmin xmax nx
-            ymin ymax ny]
-  (let [delx (/ (- xmax xmin) nx)
-        dely (/ (- ymax ymin) ny)
-        xs (range xmin xmax delx)
-        ys (range ymin ymax dely)]
-    (for [x xs y ys] (Complex. (double x) (double y)))))
-
-
-(defn map-grid [f xmin xmax nx
-                ymin ymax ny]
+(defn map-grid
+  "map complex func f over grid"
+  [f xmin xmax nx
+   ymin ymax ny]
   (let [delx (/ (- xmax xmin) nx)
         dely (/ (- ymax ymin) ny)
         xs (range xmin xmax delx)
         ys (range ymin ymax dely)]
     (for [x xs y ys]
       (f (Complex. (double x) (double y))))))
+
+(def grid (partial map-grid identity))
 
 (defn pmap-grid [f xmin xmax nx
                  ymin ymax ny]
@@ -57,20 +43,41 @@
     (doall (pmap f grd))))
 
 
-(defn to-cmap [min max]
-  ())
+;;;; some polynomial functions ;;;;;
+(defn scc [^Complex z]
+  (let [c (Complex. 0.99 0.14)]
+    (C/times z (C/plus z c))))
 
-(defn iters [n px py]
-  (let [escaped? (partial iter-while< 1E10 rabbit n)]
-    (vec (pmap-grid escaped? -1 1 px -1 1 py))))
+(defn dragonfly
+  "z^5 + (0.8+0.8i)z^3 + z"
+  [^Complex z]
+  (let [c (Complex. 0.8 0.8)
+        one (Complex. 1.0 0.0)
+        z3 (C/times z (C/times z z))]
+    (C/times z (C/plus one
+                       (C/times z3 (C/plus z c))))))
 
+(defn z2-c [^Complex c ^Complex z]
+  (C/plus (C/times z z) c))
+
+(def spiral (partial z2-c (Complex. -0.744336 0.121198)))
+
+(def rabbit (partial z2-c (Complex. -0.122 0.745)))
+
+(def airplane (partial z2-c (Complex. -1.0 0.0)))
+
+;;;; color gradients ;;;;
 (defn red-black [n itr]
   (let [i (* 100 (/ itr n))]
     (cond
-      (< i 20) (Color. 218 5 38)
-      (< i 40) (Color. 152 0 25)
-      (< i 80) (Color. 87 0 14)
+      (< i 20) (Color. 220 5 38)
+      (< i 50) (Color. 152 0 25)
+      (< i 99) (Color. 87 0 14)
       :else Color/black)))
+
+(defn iters [n px py]
+  (let [escaped? (partial iter-while< 1E9 rabbit n)]
+    (vec (pmap-grid escaped? -1 1 px -1 1 py))))
 
 (defn -main
   [n px py]
@@ -78,7 +85,7 @@
         gfx (.createGraphics img)
         its (iters n px py)]
     (doseq [x (range px) y (range py)]
-      (let [it (get its (+ (* x px) y))
+      (let [it (get its (+ (* x py) y))
             col (red-black n it)]
         (.setColor gfx col)
         (.fillRect gfx x y 1 1)))
